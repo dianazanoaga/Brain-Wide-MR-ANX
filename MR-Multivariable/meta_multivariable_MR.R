3719f6d3d3b0f79242108e6a5642ce9716068ec7
dianazanoaga <148770389+dianazanoaga@users.noreply.github.com>
2023-11-02 18:29:51 +0100

    final push

---
+++ MR-Multivariable/meta_multivariable_MR.R
@@ -0,0 +1,92 @@
+
+
+library(meta)
+library(readxl)
+library(data.table)
+library("plyr")
+library(meta)
+library(purrr)
+library(metafor)
+library(rio) # per leggere piu sheets di R
+library(WriteXLS)
+library(writexl)
+
+
+mvp_multivariable_result <- read_excel("/gpfs/gibbs/pi/polimanti/diana/mvp_multivariableMr_harm.xlsx")
+finn_multivariable_result <- read_excel("/gpfs/gibbs/pi/polimanti/diana/finn_multivariableMr_harm.xlsx")
+ukbio_multivariable_result<- read_excel("/gpfs/gibbs/pi/polimanti/diana/ukbio_multivariableMr_harm.xlsx")
+
+mvp_multivariable_result <- as.data.frame(mvp_multivariable_result)
+finn_multivariable_result <- as.data.frame(finn_multivariable_result)
+ukbio_multivariable_result <- as.data.frame(ukbio_multivariable_result)
+
+dataset <- data.frame(matrix(ncol=1, nrow= 6))
+dataset$"brain_n" <- c("0953", "0043", "1961", "1511", "1437", "3915")
+
+for(i in c(1:6)){
+  
+  dataset$"exposure"[i]  <- mvp_multivariable_result$exposure[i]
+  dataset$"mvp_n_multi"[i]  <- mvp_multivariable_result[i, 8]
+  dataset$"mvp_b_multi"[i] <- mvp_multivariable_result[i, 3]
+  dataset$"mvp_se_multi"[i] <- mvp_multivariable_result[i, 4]
+  dataset$"mvp_p"[i]  <- mvp_multivariable_result[i, 7]
+  dataset$"finn_n_multi"[i]  <- finn_multivariable_result[i, 8]
+  dataset$"finn_b_multi"[i] <- finn_multivariable_result[i, 3]
+  dataset$"finn_se_multi"[i] <- finn_multivariable_result[i, 4]
+  dataset$ "finn_p_multi"[i]  <- finn_multivariable_result[i, 7]
+  dataset$"ukbio_snp_multi"[i] <- ukbio_multivariable_result[i, 8]
+  dataset$"ukbio_b_multi"[i]  <- ukbio_multivariable_result[i, 3]
+  dataset$"ukbio_se_multi"[i]  <- ukbio_multivariable_result[i, 4]
+  dataset$"ukbio_p_multi"[i]  <- ukbio_multivariable_result[i, 7]
+  
+}
+
+dataset <- dataset[,-1]
+
+for(i in c(1:6)){
+  
+  beta <- c(dataset$mvp_b_multi[i], dataset$finn_b_multi[i], dataset$ukbio_b_multi[i])
+  se <- c(dataset$mvp_se_multi[i], dataset$finn_se_multi[i], dataset$ukbio_se_multi[i])
+  data <- data.frame(r = beta, se = se)
+  meta_result_reml <- rma.uni(r, sei = se , data = data)
+  meta_result_common <- rma.uni(r, sei = se , data = data,method= "EE")
+  meta_result_dl <- rma.uni(r, sei = se , data = data, method = "DL")
+  
+  
+  dataset$"meta_b_reml"[i] <- meta_result_reml$beta
+  dataset$"meta_se_reml"[i] <- meta_result_reml$se
+  dataset$"meta_pval_reml"[i] <- meta_result_reml$pval
+  dataset$"meta_het_reml"[i] <- meta_result_reml$QEp
+  
+  dataset$"meta_b_common_multi"[i] <- meta_result_common$beta
+  dataset$"meta_se_common_multi"[i] <- meta_result_common$se
+  dataset$"meta_pval_common_multi"[i] <- meta_result_common$pval
+  dataset$"meta_het_common_multi"[i] <- meta_result_common$QEp
+  
+  dataset$"meta_b_dl"[i] <- meta_result_dl$beta
+  dataset$"meta_se_dl"[i] <- meta_result_dl$se
+  dataset$"meta_pval_dl"[i] <- meta_result_dl$pval
+  dataset$"meta_het_dl"[i] <- meta_result_dl$QEp
+  
+}
+
+dataset$"p_adj_reml" <- p.adjust(dataset$"meta_pval_reml" , method="fdr")
+
+dataset$"p_adj_common_multi" <- p.adjust(dataset$"meta_pval_common" , method="fdr")
+
+dataset$"p_adj_dl" <- p.adjust(dataset$"meta_pval_dl" , method="fdr")
+
+dataset$"p_adj_reml_het" <- p.adjust(dataset$"meta_het_reml" , method="fdr")
+
+dataset$"p_adj_common_het_multi" <- p.adjust(dataset$"meta_het_common" , method="fdr")
+
+dataset$"p_adj_dl_het" <- p.adjust(dataset$"meta_het_dl" , method="fdr")
+
+
+write_xlsx(dataset, "/gpfs/gibbs/pi/polimanti/diana/meta_multivariable_MR_harm.xlsx")
+
+
+corr_between_6 <- read_excel("/gpfs/gibbs/pi/polimanti/diana/corr_between_6brains.xlsx")
+corr_between_6 <- as.data.frame(corr_between_6)
+corr_between_6$"p_adj" <- p.adjust(corr_between_6$p, "fdr")
+write_xlsx(corr_between_6, "/gpfs/gibbs/pi/polimanti/diana/corr_between_6brains.xlsx")
---
+++ MR-Multivariable/multivariable_MR41_mvp.R
@@ -0,0 +1,161 @@
+#!/usr/bin/env Rscript
+
+
+library(readxl)
+library(data.table)
+library("plyr")
+library("dplyr")
+library(meta)
+library(purrr)
+library(metafor)
+library(rio) # per leggere piu sheets di R
+library(WriteXLS)
+library(writexl)
+library(MendelianRandomization)
+library(tidyverse)
+library(TwoSampleMR)
+
+
+
+my_list = c("/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/0953.txt","/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/0043.txt","/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/1961.txt",
+            "/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/1511.txt","/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/1437.txt","/gpfs/gibbs/pi/polimanti/diana/files_format_nuovi/3915.txt")
+
+mvp_all <- fread("/gpfs/gibbs/pi/polimanti/diana/gad_analysis/input_gad/dbGAP_GAD2eurMVP")
+
+# read all the brains
+for(i in 1:length(my_list)) {  # assign function within loop
+  
+  brain <- fread(my_list[i])
+  #brain_sig_relaxed <- brain[brain$pvalue < 1*10^(-5)]
+  assign(paste0("brain_", i), format_data(brain, 
+                                          type="exposure", 
+                                          phenotype_col = "brain",
+                                          snp_col = "rsid",
+                                          beta_col = "beta",
+                                          se_col = "se",
+                                          effect_allele_col = "a2",
+                                          other_allele_col = "a1",
+                                          pval_col = "pvalue"))
+}
+
+
+
+# find snps where at least one is less tha threshold significant 
+common_rows <- intersect(intersect(intersect(intersect(intersect(brain_1$SNP, brain_3$SNP), brain_4$SNP), brain_5$SNP), brain_2$SNP), brain_6$SNP) #0
+
+dt_all <- as.data.frame(matrix(ncol=13))
+colnames(dt_all) <- colnames(brain_2)
+
+for(i in c(1:1205886)){
+  print(i)
+  x = min(brain_1$pval.exposure[i], brain_2$pval.exposure[i],brain_3$pval.exposure[i],
+          brain_4$pval.exposure[i], brain_5$pval.exposure[i],brain_6$pval.exposure[i])
+  
+  if(x < 1*10^(-5)){
+    
+    if(x == brain_1$pval.exposure[i]){
+      dt_all <- rbind(dt_all, brain_1[i,])
+    } else if(x == brain_2$pval.exposure[i]){
+      dt_all <- rbind(dt_all, brain_2[i,])
+    } else if(x == brain_3$pval.exposure[i]){
+      dt_all <- rbind(dt_all, brain_3[i,])
+    } else if(x == brain_4$pval.exposure[i]){
+      dt_all <- rbind(dt_all, brain_4[i,])
+    } else if(x == brain_4$pval.exposure[i]){
+      dt_all <- rbind(dt_all, brain_5[i,])
+    } else{
+      dt_all <- rbind(dt_all, brain_6[i,])
+    }
+  }
+}
+
+
+dt_all_sig <- unique(dt_all[dt_all$pval.exposure < 1*10^(-5),])
+
+# clump data!
+dt_all_sig_clump <- clump_data(dt_all_sig)
+write_xlsx(dt_all_sig_clump, "/gpfs/gibbs/pi/polimanti/diana/multivariable_exp_input_clump_harm.xlsx")
+
+
+# take only the selected snps from the original dataset
+for(i in 1:6){
+  
+assign(paste0("brain_selected_snps_", i), get(paste0("brain_", i))[get(paste0("brain_", i))$SNP %in% dt_all_sig_clump$SNP,])
+}
+
+# select these snps also from the mvp and HARMONIZE
+for(i in 1:6) {
+  
+  print(i)
+  assign(paste0("mvp_out_",i), read_outcome_data(
+    snps = get(paste0("brain_selected_snps_", i))$SNP,
+    sep = ",",
+    filename = "/gpfs/gibbs/pi/polimanti/diana/mvp_all.txt",
+    snp_col = "rsid",
+    beta_col = "EFFECT",
+    se_col = "SE",
+    effect_allele_col = "A1",
+    other_allele_col = "A2",
+    pval_col = "P",
+    samplesize_col = "N"))
+  
+  
+  assign(paste0("b_mvp_",i, "_har"), harmonise_data(
+    exposure_dat = get(paste0("brain_selected_snps_", i)), 
+    outcome_dat = get(paste0("mvp_out_", i))
+    
+  ))
+}
+
+# Take only colonne che vogliamo
+for(i in 1:6){
+assign(paste0("b_mvp_short_", i, "_har"), get(paste0("b_mvp_", i, "_har"))[,c(1:7, 14, 15, 22, 23)])
+}
+
+
+common_rows_new <- list(b_mvp_short_1_har, b_mvp_short_2_har, b_mvp_short_3_har,
+                    b_mvp_short_4_har, b_mvp_short_5_har, b_mvp_short_6_har)
+
+common_rows_new <- as.data.frame(common_rows_new)
+
+# START MULTIVARIABLE
+
+m1 = matrix(common_rows_new$beta.exposure)
+m2 =matrix(common_rows_new$beta.exposure.1)
+m3 =matrix(common_rows_new$beta.exposure.2)
+m4 =matrix(common_rows_new$beta.exposure.3)
+m5 =matrix(common_rows_new$beta.exposure.4)
+m6 = matrix(common_rows_new$beta.exposure.5)
+
+m1s = matrix(common_rows_new$se.exposure)
+m2s =matrix(common_rows_new$se.exposure.1)
+m3s =matrix(common_rows_new$se.exposure.2)
+m4s =matrix(common_rows_new$se.exposure.3)
+m5s =matrix(common_rows_new$se.exposure.4)
+m6s = matrix(common_rows_new$se.exposure.5)
+
+
+
+MRMVInputObject <- mr_mvinput(bx = cbind(m1,m2,m3,m4,m5,m6),
+                              bxse = cbind(m1s, m2s, m3s, m4s, m5s, m6s),
+                              by = common_rows_new$beta.outcome,
+                              byse = common_rows_new$se.outcome,
+                              snps = common_rows_new$SNP)
+
+MR_multi <- mr_mvivw(MRMVInputObject)
+MR_multi
+
+mvp_multivariable_result <- data.frame("exposure" = MR_multi@Exposure, 
+                                       "outcome" = rep("mvp", 6),
+                                       "estimate" = MR_multi@Estimate,
+                                       "std error" = MR_multi@StdError,
+                                       "low_CI" = MR_multi@CILower,
+                                       "high_CI" = MR_multi@CIUpper,
+                                       "pvalue" = MR_multi@Pvalue,
+                                       "snp" = rep(MR_multi@SNPs,6))
+
+write_xlsx(mvp_multivariable_result, "/gpfs/gibbs/pi/polimanti/diana/mvp_multivariableMr_harm.xlsx")
+
+
+
+
