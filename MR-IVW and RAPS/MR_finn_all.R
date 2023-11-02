834ffe3cf0a20561f25d6c0eae891a8fae3d45ff
dianazanoaga <148770389+dianazanoaga@users.noreply.github.com>
2023-11-02 21:59:47 +0100

    final push

--- MR-IVW and RAPS/MR_finn_all.R
+++ MR-IVW and RAPS/MR_finn_all.R
@@ -16,26 +16,23 @@ library(mr.raps)
 library(WriteXLS)
 library(writexl)
 
-
-
-# load necessary datasets
+# Load necessary datasets
 
 finn_all <- fread("/gpfs/gibbs/pi/polimanti/diana/finn_all.txt")
-
 
 batch_args <- read_excel(args[1])
 batch_args <- t(batch_args)
 print(seq_along(batch_args))
 
-
 for (i in seq_along(batch_args)) {
+
   print(i)
   print(batch_args[i])
 
   brain_all <- fread(batch_args[i])
   brain_all <- as.data.frame(brain_all)
   
-  # select only significant SNPs for both datasets
+  # Select only significant SNPs for both datasets
   finn_sig <- finn_all[finn_all$pval < 5*10^(-8),]
   finn_sig_relaxed <- finn_all[finn_all$pval < 1*10^(-5),]
   
@@ -43,8 +40,10 @@ for (i in seq_along(batch_args)) {
   brain_sig_relaxed <- brain_all[brain_all$pvalue < 1*10^(-5),]
   
   
-  # formatting 
+  # Formatting 
+
   if(dim(finn_sig)[1] != 0 ){
+
     finn_for <- format_data(finn_sig, 
                             type="exposure", 
                             phenotype_col = "anxiety",
@@ -57,8 +56,7 @@ for (i in seq_along(batch_args)) {
                             pval_col = "pval")
   }
   
-  
-  finn_for_relaxed <- format_data(finn_sig_relaxed, 
+    finn_for_relaxed <- format_data(finn_sig_relaxed, 
                                  type="exposure", 
                                  phenotype_col = "anxiety",
                                  snp_col = "rsids",
@@ -70,6 +68,7 @@ for (i in seq_along(batch_args)) {
                                  pval_col = "pval")
   
   if(dim(brain_sig)[1] != 0 ){
+
     brain_for <- format_data(brain_sig, 
                              type="exposure", 
                              phenotype_col = "brain",
@@ -82,7 +81,7 @@ for (i in seq_along(batch_args)) {
                              samplesize_col = "n")
   }
   
-  brain_for_relaxed <- format_data(brain_sig_relaxed, 
+    brain_for_relaxed <- format_data(brain_sig_relaxed, 
                                    type="exposure", 
                                    phenotype_col = "brain",
                                    snp_col = "rsid",
@@ -95,34 +94,39 @@ for (i in seq_along(batch_args)) {
   
   # CLUMPING
   
-  #clumping anxiety
+  # Clumping anxiety
   
   if(dim(finn_sig)[1] != 0 ){
+
     finn_clump <- finn_for
     finn_clump <- clump_data(finn_clump)
+
   }
   
   finn_clump_relaxed <- finn_for_relaxed
   finn_clump_relaxed <- clump_data(finn_clump_relaxed)
   
-  # clumping brain
+  # Clumping brain IDP
+
   if(dim(brain_sig)[1] != 0 ){
+
     brain_clump <- brain_for
     brain_clump <- clump_data(brain_clump)
+
   }
   
   brain_clump_relaxed <- brain_for_relaxed
   brain_clump_relaxed <- clump_data(brain_clump_relaxed)
   
-  
   merge_brain_out <- merge(x=finn_clump, y=brain_all, by.x= "SNP", by.y = "rsid")
   merge_finn_out <- merge(x=brain_clump, y=finn_all, by.x= "SNP", by.y = "rsids")
   
-  # anx as exposure and brain as outcome
+  # ANX as exposure and Brain IDP as outcome
   
   if((dim(finn_sig)[1] != 0) & (dim(merge_brain_out)[1]!= 0)){
     
     brain_out <- read_outcome_data(
+
       snps = finn_clump$SNP,
       sep = ",",
       filename = batch_args[i],
@@ -133,27 +137,32 @@ for (i in seq_along(batch_args)) {
       other_allele_col = "a1",
       pval_col = "pvalue",
       samplesize_col = "n"
-    )
-  } else {
+
+    )} else {
+
     brain_out = matrix(c(0,0,0,0), nrow=1, ncol=1)
+
   }
   
-  brain_out_relaxed <- read_outcome_data(
-    snps = finn_clump_relaxed$SNP,
-    sep = ",",
-    filename = batch_args[i],
-    snp_col = "rsid",
-    beta_col = "beta",
-    se_col = "se",
-    effect_allele_col = "a2",
-    other_allele_col = "a1",
-    pval_col = "pvalue",
-    samplesize_col = "n"
+    brain_out_relaxed <- read_outcome_data(
+
+      snps = finn_clump_relaxed$SNP,
+      sep = ",",
+      filename = batch_args[i],
+      snp_col = "rsid",
+      beta_col = "beta",
+      se_col = "se",
+      effect_allele_col = "a2",
+      other_allele_col = "a1",
+      pval_col = "pvalue",
+      samplesize_col = "n"
+
   )
   
   if((dim(brain_sig)[1] != 0) & (dim(merge_finn_out)[1]!= 0)){
     
-      finn_out <- read_outcome_data(
+    finn_out <- read_outcome_data(
+
       snps = brain_clump$SNP,
       sep = ",",
       filename = "/gpfs/gibbs/pi/polimanti/diana/finn_all.txt",
@@ -163,63 +172,60 @@ for (i in seq_along(batch_args)) {
       effect_allele_col = "alt",
       other_allele_col = "ref",
       pval_col = "pval"
-    )
-  } else {
+
+    )} else {
+
     finn_out = matrix(c(0,0,0,0), nrow=1, ncol=1)
+
   }
   
-  finn_out_relaxed <- read_outcome_data(
-    snps = brain_clump_relaxed$SNP,
-    sep = ",",
-    filename = "/gpfs/gibbs/pi/polimanti/diana/finn_all.txt",
-    snp_col = "rsids",
-    beta_col = "beta",
-    se_col = "sebeta",
-    effect_allele_col = "alt",
-    other_allele_col = "ref",
-    pval_col = "pval"
+    finn_out_relaxed <- read_outcome_data(
+
+      snps = brain_clump_relaxed$SNP,
+      sep = ",",
+      filename = "/gpfs/gibbs/pi/polimanti/diana/finn_all.txt",
+      snp_col = "rsids",
+      beta_col = "beta",
+      se_col = "sebeta",
+      effect_allele_col = "alt",
+      other_allele_col = "ref",
+      pval_col = "pval"
+
   )
   
+  # Harmoniza for ANX as exposure and Brain IDP as outcome
+  
+  anx_brain_harmonize_relaxed <- harmonise_data(
+    exposure_dat = finn_clump_relaxed, 
+    outcome_dat = brain_out_relaxed
+    )
+    
+    # Perform Mendelian Randomization
 
-  mr_method_list()$name
+  mr_anx_brain_relaxed <- mr(anx_brain_harmonize_relaxed, method_list = mr_method_list()$obj)
+  mr_anx_brain_relaxed$"exposure" = "finn anxiety"
+  mr_anx_brain_relaxed$"outcome" =  batch_args[i]
+  n <- dim(mr_anx_brain_relaxed)[1]
+  mr_anx_brain_relaxed$"threshold" = rep("relaxed threshold", n)
+    
+  list_data_1 <- mr_anx_brain_relaxed
+    
+  # Harmonize for Brain IDP as exposure and ANX as outcome
   
-  # harmoniza for anx as exposure and brain as outcome
-  
-    anx_brain_harmonize_relaxed <- harmonise_data(
-      exposure_dat = finn_clump_relaxed, 
-      outcome_dat = brain_out_relaxed
+  brain_anx_harmonize_relaxed <- harmonise_data(
+    exposure_dat = brain_clump_relaxed, 
+    outcome_dat = finn_out_relaxed
     )
     
-    #mendelian 
-    mr_anx_brain_relaxed <- mr(anx_brain_harmonize_relaxed, method_list = mr_method_list()$obj)
-    mr_anx_brain_relaxed$"exposure" = "finn anxiety"
-    mr_anx_brain_relaxed$"outcome" =  batch_args[i]
-    n <- dim(mr_anx_brain_relaxed)[1]
-    mr_anx_brain_relaxed$"threshold" = rep("relaxed threshold", n)
+    # Perfrom Mendelian Randomization
+
+  mr_brain_anx_relaxed <- mr(brain_anx_harmonize_relaxed, method_list = mr_method_list()$obj)
+  mr_brain_anx_relaxed$"exposure" =  batch_args[i]
+  mr_brain_anx_relaxed$"outcome" =  "finn anxiety"
+  n <- dim(mr_brain_anx_relaxed)[1]
+  mr_brain_anx_relaxed$"threshold" = rep("relaxed threshold", n)
     
-    list_data_1 <- mr_anx_brain_relaxed
-    
-    
-  
-  
-  
-  # harmonize for brain as exposure and anx as outcome
-  
-   
-    brain_anx_harmonize_relaxed <- harmonise_data(
-      exposure_dat = brain_clump_relaxed, 
-      outcome_dat = finn_out_relaxed
-    )
-    
-    #mendelian 
-    mr_brain_anx_relaxed <- mr(brain_anx_harmonize_relaxed, method_list = mr_method_list()$obj)
-    mr_brain_anx_relaxed$"exposure" =  batch_args[i]
-    mr_brain_anx_relaxed$"outcome" =  "finn anxiety"
-    n <- dim(mr_brain_anx_relaxed)[1]
-    mr_brain_anx_relaxed$"threshold" = rep("relaxed threshold", n)
-    
-    list_data_2 <- mr_brain_anx_relaxed
-    
+  list_data_2 <- mr_brain_anx_relaxed
   
   list_data <- list("anx_brain" = list_data_1 ,"brain_anx" = list_data_2)
   
--- MR-IVW and RAPS/MR_mvp_all.R
+++ MR-IVW and RAPS/MR_mvp_all.R
@@ -15,43 +15,43 @@ library(mr.raps)
 library(WriteXLS)
 library(writexl)
 
-
-
-# load necessary datasets
+# Load necessary datasets
 
 mvp_all <- fread("/gpfs/gibbs/pi/polimanti/diana/mvp_all.txt")
-
 
 batch_args <- read_excel(args[1])
 batch_args <- t(batch_args)
 print(seq_along(batch_args))
 
 for (i in seq_along(batch_args)) {
+
  print(i)
  print(batch_args[i])
  brain_all <- fread(batch_args[i])
  brain_all <- as.data.frame(brain_all)
 
-# select only significant SNPs for both datasets
+# Select only significant SNPs for both datasets
  mvp_sig <- mvp_all[mvp_all$P < 5*10^(-8),]
  mvp_sig_relaxed <- mvp_all[mvp_all$P < 1*10^(-5),]
 
  brain_sig <- brain_all[brain_all$pvalue < 5*10^(-8),]
  brain_sig_relaxed <- brain_all[brain_all$pvalue < 1*10^(-5),]
 
+# Formatting 
 
-# formatting 
  if(dim(mvp_sig)[1] != 0 ){
- mvp_for <- format_data(mvp_sig, 
-                       type="exposure", 
-                       phenotype_col = "mvp_anxiety",
-                       snp_col = "rsid",
-                       beta_col = "EFFECT",
-                       se_col = "SE",
-                       eaf_col = "Freq1",
-                       effect_allele_col = "A1",
-                       other_allele_col = "A2",
-                       pval_col = "P")
+
+    mvp_for <- format_data(mvp_sig, 
+                          type="exposure", 
+                          phenotype_col = "mvp_anxiety",
+                          snp_col = "rsid",
+                          beta_col = "EFFECT",
+                          se_col = "SE",
+                          eaf_col = "Freq1",
+                          effect_allele_col = "A1",
+                          other_allele_col = "A2",
+                          pval_col = "P")
+
  }
  
  mvp_for_relaxed <- format_data(mvp_sig_relaxed, 
@@ -66,16 +66,17 @@ for (i in seq_along(batch_args)) {
                                pval_col = "P")
  
  if(dim(brain_sig)[1] != 0 ){
- brain_for <- format_data(brain_sig, 
-                         type="exposure", 
-                         phenotype_col = "brain_0705",
-                         snp_col = "rsid",
-                         beta_col = "beta",
-                         se_col = "se",
-                         effect_allele_col = "a2",
-                         other_allele_col = "a1",
-                         pval_col = "pvalue",
-                         samplesize_col = "n")
+
+    brain_for <- format_data(brain_sig, 
+                          type="exposure", 
+                          phenotype_col = "brain_0705",
+                          snp_col = "rsid",
+                          beta_col = "beta",
+                          se_col = "se",
+                          effect_allele_col = "a2",
+                          other_allele_col = "a1",
+                          pval_col = "pvalue",
+                          samplesize_col = "n")
  }
  
  brain_for_relaxed <- format_data(brain_sig_relaxed, 
@@ -91,30 +92,34 @@ for (i in seq_along(batch_args)) {
 
 # CLUMPING
 
-#clumping anxiety
+# Clumping anxiety
  
  if(dim(mvp_sig)[1] != 0 ){
+
  mvp_clump <- mvp_for
  mvp_clump <- clump_data(mvp_clump)
+
  }
  
  mvp_clump_relaxed <- mvp_for_relaxed
  mvp_clump_relaxed <- clump_data(mvp_clump_relaxed)
 
-# clumping brain
+# Clumping brain
+
  if(dim(brain_sig)[1] != 0 ){
+
  brain_clump <- brain_for
  brain_clump <- clump_data(brain_clump)
+
  }
  
  brain_clump_relaxed <- brain_for_relaxed
  brain_clump_relaxed <- clump_data(brain_clump_relaxed)
 
-
-
-# anx as exposure and brain as outcome
+# ANX as exposure and Brain IDP as outcome
  
  if(dim(mvp_sig)[1] != 0 ){
+
  brain_out <- read_outcome_data(
   snps = mvp_clump$SNP,
   sep = ",",
@@ -126,12 +131,15 @@ for (i in seq_along(batch_args)) {
   other_allele_col = "a1",
   pval_col = "pvalue",
   samplesize_col = "n"
-)
- } else {
+
+)} else {
+
    brain_out = matrix(c(0,0,0,0), nrow=1, ncol=1)
+
 }
 
  brain_out_relaxed <- read_outcome_data(
+
   snps = mvp_clump_relaxed$SNP,
   sep = ",",
   filename = batch_args[i],
@@ -142,26 +150,31 @@ for (i in seq_along(batch_args)) {
   other_allele_col = "a1",
   pval_col = "pvalue",
   samplesize_col = "n"
+
 )
  
  if(dim(brain_sig)[1] != 0 ){
- mvp_out <- read_outcome_data(
-  snps = brain_clump$SNP,
-  sep = ",",
-  filename = "/gpfs/gibbs/pi/polimanti/diana/mvp_all.txt",
-  snp_col = "rsid",
-  beta_col = "EFFECT",
-  se_col = "SE",
-  effect_allele_col = "A1",
-  other_allele_col = "A2",
-  pval_col = "P",
-  samplesize_col = "N"
-)
- } else{
+
+  mvp_out <- read_outcome_data(
+    snps = brain_clump$SNP,
+    sep = ",",
+    filename = "/gpfs/gibbs/pi/polimanti/diana/mvp_all.txt",
+    snp_col = "rsid",
+    beta_col = "EFFECT",
+    se_col = "SE",
+    effect_allele_col = "A1",
+    other_allele_col = "A2",
+    pval_col = "P",
+    samplesize_col = "N"
+
+  )} else{
+
    mvp_out = matrix(c(0,0,0,0), nrow=1, ncol=1)
+
  }
  
  mvp_out_relaxed <- read_outcome_data(
+
   snps = brain_clump_relaxed$SNP,
   sep = ",",
   filename = "/gpfs/gibbs/pi/polimanti/diana/mvp_all.txt",
@@ -172,53 +185,53 @@ for (i in seq_along(batch_args)) {
   other_allele_col = "A2",
   pval_col = "P",
   samplesize_col = "N"
+
 )
 
 mr_method_list()$name
 
-# harmoniza for anx as exposure and brain as outcome
+# Harmonize for ANX as exposure and Brain IDP as outcome
 
- #if(dim(brain_out)[1] <5) {
-  
-    anx_brain_harmonize_relaxed <- harmonise_data(
+anx_brain_harmonize_relaxed <- harmonise_data(
+
     exposure_dat = mvp_clump_relaxed, 
     outcome_dat = brain_out_relaxed
+
+  )    
+  
+# Perform Mendelian Randomization
+
+mr_anx_brain_relaxed <- mr(anx_brain_harmonize_relaxed, method_list = mr_method_list()$obj)
+mr_anx_brain_relaxed$"exposure" = "mvp anxiety"
+mr_anx_brain_relaxed$"outcome" =  batch_args[i]
+n <- dim(mr_anx_brain_relaxed)[1]
+mr_anx_brain_relaxed$"threshold" = rep("relaxed threshold", n)
+  
+list_data_1 <- mr_anx_brain_relaxed    
+  
+# Harmonize for Brain IDP as exposure and ANX as outcome
+
+brain_anx_harmonize_relaxed <- harmonise_data(
+
+  exposure_dat = brain_clump_relaxed, 
+  outcome_dat = mvp_out_relaxed
+
   )
   
-  #mendelian 
-    mr_anx_brain_relaxed <- mr(anx_brain_harmonize_relaxed, method_list = mr_method_list()$obj)
-    mr_anx_brain_relaxed$"exposure" = "mvp anxiety"
-    mr_anx_brain_relaxed$"outcome" =  batch_args[i]
-    n <- dim(mr_anx_brain_relaxed)[1]
-    mr_anx_brain_relaxed$"threshold" = rep("relaxed threshold", n)
-  
-    list_data_1 <- mr_anx_brain_relaxed
-  
-  
+  # Perform Mendelian Randomization
 
-
-# harmonize for brain as exposure and anx as outcome
-
+mr_brain_anx_relaxed <- mr(brain_anx_harmonize_relaxed, method_list = mr_method_list()$obj)
+mr_brain_anx_relaxed$"exposure" =  batch_args[i]
+mr_brain_anx_relaxed$"outcome" =  "mvp anxiety"
+n <- dim(mr_brain_anx_relaxed)[1]
+mr_brain_anx_relaxed$"threshold" = rep("relaxed threshold", n)
   
-    brain_anx_harmonize_relaxed <- harmonise_data(
-    exposure_dat = brain_clump_relaxed, 
-    outcome_dat = mvp_out_relaxed
-  )
-  
-  #mendelian 
-    mr_brain_anx_relaxed <- mr(brain_anx_harmonize_relaxed, method_list = mr_method_list()$obj)
-    mr_brain_anx_relaxed$"exposure" =  batch_args[i]
-    mr_brain_anx_relaxed$"outcome" =  "mvp anxiety"
-    n <- dim(mr_brain_anx_relaxed)[1]
-    mr_brain_anx_relaxed$"threshold" = rep("relaxed threshold", n)
-  
-   list_data_2 <- mr_brain_anx_relaxed
-  
+list_data_2 <- mr_brain_anx_relaxed
 
- list_data <- list("anx_brain" = list_data_1 ,"brain_anx" = list_data_2)
+list_data <- list("anx_brain" = list_data_1 ,"brain_anx" = list_data_2)
 
- id <- sub(".txt.*", "", batch_args[i])
- id <- sub(".*/gpfs/gibbs/pi/polimanti/diana/files_format", "", id)
- write_xlsx(list_data, paste0("/gpfs/gibbs/pi/polimanti/diana/MR_results/mvp", id, ".xlsx"))
+id <- sub(".txt.*", "", batch_args[i])
+id <- sub(".*/gpfs/gibbs/pi/polimanti/diana/files_format", "", id)
+write_xlsx(list_data, paste0("/gpfs/gibbs/pi/polimanti/diana/MR_results/mvp", id, ".xlsx"))
 
 }
